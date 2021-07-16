1f117db14cdb50b4062d94fceb5ddc39012232b0
patrick <chodowski.patrick@chalhoub.com>
2021-07-16 18:12:53 +0400

    added screen manager

--- app.py
+++ app.py
@@ -3,23 +3,52 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.core.window import Window
+from youtube_converter import Youtube
+import os
+from kivy.core.audio import SoundLoader
+from kivy.uix.filechooser import FileChooser, FileChooserListView
+
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 Window.size = (400, 500)
-Builder.load_file('music.kv')
+y = Youtube()
 
 
-class PrimaryScreen(MDScreen):
+class DownloadScreen(MDScreen):
     def __init__(self, **kwargs):
-        super(PrimaryScreen, self).__init__(**kwargs)
+        super(DownloadScreen, self).__init__(**kwargs)
     youtube_link = ObjectProperty(None)
 
-    def press_submit(self):
-        print(f"link: {self.youtube_link.text}")
+    def download_youtube_url(self) -> None:
+        url = self.youtube_link.text
+        print(f"link: {url}")
+
+        if 'https://www.youtube.com/watch?v=' in url:
+            y.get_best_audio(url=url)
+
+
+class SongPlayerScreen(MDScreen):
+    def __init__(self, **kwargs):
+        super(SongPlayerScreen, self).__init__(**kwargs)
+
+    def list_music(self) -> list:
+        song_list = list()
+        for file in os.listdir('./Downloads'):
+            if file.endswith(".m4a"):
+                song_list.append(file)
+        print(song_list)
+        return song_list
+
+
+class Screens(ScreenManager):
+    pass
 
 
 class MusicApp(MDApp):
     def build(self):
-        return PrimaryScreen()
+        kv = Builder.load_file('music.kv')
+        return kv
+        #return PrimaryScreen()
 
 
 if __name__ == "__main__":
--- youtube_converter.py
+++ youtube_converter.py
@@ -1,20 +1,16 @@
 import subprocess
 
-url = 'https://www.youtube.com/watch?v=Mbsc-3T6o4M'
-
-
 class Youtube:
     def __init__(self):
         pass
 
     def get_best_audio(self,
                        url: str,
-                       destination_path: str = ''
+                       destination_path: str = '~/Documents/projects/kivyfun/downloads'
                        ):
         """
         Get new access token and headers
         """
-        destination_path = '~/Documents/projects/kivyfun/downloads'
         #cmd = "youtube-dl -f 'bestaudio[ext=m4a]' 'https://www.youtube.com/watch?v=Mbsc-3T6o4M'"
         cmd = f"youtube-dl -o '{destination_path}/%(title)s.%(ext)s' -f 'bestaudio[ext=m4a]' '{url}'"
 
@@ -23,6 +19,3 @@ class Youtube:
         #cmd = f"youtube-dl -x --audio-format mp3 'https://www.youtube.com/watch?v=Mbsc-3T6o4M'"
         subprocess.run(cmd, shell=True)
 
-
-yt = Youtube()
-yt.get_best_audio(url)
